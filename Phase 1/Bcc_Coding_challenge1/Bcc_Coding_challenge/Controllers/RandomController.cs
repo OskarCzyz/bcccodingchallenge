daf5eca0431cd65114986e8d49530560939e3e6d
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2022-10-17 12:34:19 +0200

    to deploy

--- Phase 1/Bcc_Coding_challenge1/Bcc_Coding_challenge/Controllers/RandomController.cs
+++ Phase 1/Bcc_Coding_challenge1/Bcc_Coding_challenge/Controllers/RandomController.cs
@@ -41,7 +41,7 @@ namespace Bcc_Coding_challenge.Controllers
                 
             DataTable table = new DataTable();
             
-            Console.WriteLine("witam wszystkich chyba? nie wiem");
+            Console.WriteLine("witam wszystkich chyba? nie maybe");
             NpgsqlDataReader myReader;
             using (NpgsqlConnection myCon = new NpgsqlConnection(Environment.GetEnvironmentVariable("WHOLE_LINK")))
             {
