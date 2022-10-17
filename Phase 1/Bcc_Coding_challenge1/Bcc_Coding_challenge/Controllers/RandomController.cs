817de51f75267c75cc2240bd70297128be58338d
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2022-10-17 12:59:25 +0200

    blad

--- Phase 1/Bcc_Coding_challenge1/Bcc_Coding_challenge/Controllers/RandomController.cs
+++ Phase 1/Bcc_Coding_challenge1/Bcc_Coding_challenge/Controllers/RandomController.cs
@@ -41,7 +41,7 @@ namespace Bcc_Coding_challenge.Controllers
                 
             DataTable table = new DataTable();
             
-            Console.WriteLine("witam wszystkich chyba? lol nie");
+            Console.WriteLine("witam wszystkich chyba? blad nie");
             NpgsqlDataReader myReader;
             using (NpgsqlConnection myCon = new NpgsqlConnection(Environment.GetEnvironmentVariable("WHOLE_LINK")))
             {
