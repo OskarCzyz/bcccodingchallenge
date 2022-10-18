19135a39dce0210f334cc71d5a1817949880e763
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2022-10-18 09:49:03 +0200

    test test

--- Phase 3/server.js
+++ Phase 3/server.js
@@ -23,6 +23,10 @@ app.get('/test', (request, response) => {
   response.json({ number: `${Math.floor(Math.random() * 2147483647)}` })
 })
 
+app.get('/loaderio-53ea7526e9151fc1384f1cec39b9c739', (request, response) => {
+  response("loaderio-53ea7526e9151fc1384f1cec39b9c73")
+})
+
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
   })
