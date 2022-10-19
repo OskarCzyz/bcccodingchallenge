78cb4daf74fc5674f3aabc3b657393c1ea698bb4
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2022-10-19 09:38:04 +0200

    test

--- Phase 1/Bcc_Coding_challenge1/Bcc_Coding_challenge/Controllers/RandomController.cs
+++ Phase 1/Bcc_Coding_challenge1/Bcc_Coding_challenge/Controllers/RandomController.cs
@@ -1,4 +1,5 @@
 ﻿using Bcc_Coding_challenge.Models;
+using Google.Cloud.Diagnostics.AspNetCore;
 using Google.Cloud.Diagnostics.Common;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
@@ -12,37 +13,58 @@ namespace Bcc_Coding_challenge.Controllers
 {
 
 
+    public class TraceSamplesConstructorController : Controller
+    {
+        private readonly IManagedTracer _tracer;
+
+        /// <summary>
+        /// The <see cref="IManagedTracer"/> is populated by dependency injection.
+        /// </summary>
+        public TraceSamplesConstructorController(IManagedTracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        public void TraceHelloWorld(string id)
+        {
+            // Change the name of the span to what makese sense in your context.
+            using (_tracer.StartSpan(id))
+            {
+                // The code whose execution is to be included in the span goes here.
+                ViewData["Message"] = "Hello World.";
+            }
+        }
+    }
+
     [Route("random")]
 
     public class RandomController : Controller
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Replace ProjectId with your Google Cloud Project ID.
+            services.AddGoogleTraceForAspNetCore(new AspNetCoreTraceOptions
+            {
+                ServiceOptions = new Google.Cloud.Diagnostics.Common.TraceServiceOptions
+                {
+                    ProjectId = "Bcc_Coding_challenge"
+                }
+            });
+
+            // Add any other services your application requires, for instance,
+            // depending on the version of ASP.NET Core you are using, you may
+            // need one of the following:
+
+            // services.AddMvc();
+
+            // services.AddControllersWithViews();
+        }
+
         private readonly IConfiguration _configuration;
 
         public RandomController(IConfiguration configuration)
         {
             _configuration = configuration;
-        }
-        public class TraceSamplesConstructorController : Controller
-        {
-            private readonly IManagedTracer _tracer;
-
-            /// <summary>
-            /// The <see cref="IManagedTracer"/> is populated by dependency injection.
-            /// </summary>
-            public TraceSamplesConstructorController(IManagedTracer tracer)
-            {
-                _tracer = tracer;
-            }
-
-            public void TraceHelloWorld(string id)
-            {
-                // Change the name of the span to what makese sense in your context.
-                using (_tracer.StartSpan(id))
-                {
-                    // The code whose execution is to be included in the span goes here.
-                    ViewData["Message"] = "Hello World.";
-                }
-            }
         }
 
         /*[HttpGet(Name="random")]
