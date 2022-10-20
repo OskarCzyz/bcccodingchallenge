619ad5dade9598b762233e322a0903a91c2d7eb7
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2022-10-20 08:47:46 +0200

    final

--- Phase 2/demo/src/views/HomeView.vue
+++ Phase 2/demo/src/views/HomeView.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="home">
-    This is home page!
+    <h1>This is main page!</h1>
+    <small>It isn't really a main page but i use it for the purposue of debugging</small>
+    <small>So if you want to see the real main page click on the RANDOM button on top of your screen</small><br>
+    <div>Test 15:08</div>
   </div>
-  <div>Jest 15:08</div>
 </template>
 
 <script>
-
-
-export default {
-  
-}
+export default {};
 </script>
